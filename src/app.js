6110ea65b31b04c1eb4b2961da39e1ef1662a081
vmncASIR <victormartineznc@gmail.com>
2026-04-30 09:01:19 +0200

    feat: añadir estilos CSS y corregir log de versión

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 // Función principal de la aplicación
 function iniciar() {
-  console.log("Aplicación iniciada");
+  console.log("Aplicación iniciada - v1.1");
+  mostrarVersion("1.1");
+}
+
+function mostrarVersion(v) {
+  console.log("Versión actual: " + v);
 }
 
 iniciar();
